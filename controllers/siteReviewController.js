402a2463eab0eb18589c7d367a3481f1f0774dcf
GemyCode <abdallagamalph@gmail.com>
2025-08-26 10:19:14 +0300

    site user name update

--- controllers/siteReviewController.js
+++ controllers/siteReviewController.js
@@ -1,4 +1,5 @@
 const SiteReview = require('../models/siteReview');
+const User = require('../models/user');
 
 exports.createOrUpdateSiteReview = async (req, res, next) => {
   try {
@@ -6,9 +7,13 @@ exports.createOrUpdateSiteReview = async (req, res, next) => {
     if (!ratingValue || !description) return res.status(400).json({ message: 'ratingValue and description are required' });
     if (ratingValue < 1 || ratingValue > 5) return res.status(400).json({ message: 'ratingValue must be 1-5' });
 
+    // Fetch user to get the username
+    const user = await User.findById(req.user.id).select('username');
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
     const review = await SiteReview.findOneAndUpdate(
       { userId: req.user.id },
-      { $set: { username: req.user.username || req.user.id, ratingValue, description } },
+      { $set: { username: user.username, ratingValue, description } },
       { new: true, upsert: true, setDefaultsOnInsert: true }
     );
     res.status(201).json(review);
