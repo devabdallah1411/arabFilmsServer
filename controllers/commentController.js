8255bfc721e3cc09705961aacb1f73dd2fc4158b
Abdallah Gamal <abdallagamalph@gmail.com>
2025-12-09 09:51:10 +0200

    last

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -7,7 +7,9 @@ exports.addComment = async (req, res, next) => {
     if (!workId || !commentText) return res.status(400).json({ message: 'workId and commentText are required' });
     const work = await Work.findById(workId).select('_id createdBy');
     if (!work) return res.status(404).json({ message: 'Work not found' });
-    const comment = await Comment.create({ userId: req.user.id, workId, commentText });
+    let comment = await Comment.create({ userId: req.user.id, workId, commentText });
+    // populate user info before returning so frontend can show author
+    comment = await Comment.findById(comment._id).populate('userId', 'username');
     res.status(201).json(comment);
   } catch (err) {
     next(err);
@@ -17,7 +19,7 @@ exports.addComment = async (req, res, next) => {
 exports.getCommentsForWork = async (req, res, next) => {
   try {
     const { workId } = req.params;
-    const comments = await Comment.find({ workId }).sort({ createdAt: -1 });
+    const comments = await Comment.find({ workId }).sort({ createdAt: -1 }).populate('userId', 'username');
     res.json(comments);
   } catch (err) {
     next(err);
@@ -27,7 +29,7 @@ exports.getCommentsForWork = async (req, res, next) => {
 exports.getCommentsForPublisherWorks = async (req, res, next) => {
   try {
     const workIds = await Work.find({ createdBy: req.user.id }).distinct('_id');
-    const comments = await Comment.find({ workId: { $in: workIds } }).sort({ createdAt: -1 });
+    const comments = await Comment.find({ workId: { $in: workIds } }).sort({ createdAt: -1 }).populate('userId', 'username');
     res.json(comments);
   } catch (err) {
     next(err);
@@ -36,7 +38,7 @@ exports.getCommentsForPublisherWorks = async (req, res, next) => {
 
 exports.getAllComments = async (req, res, next) => {
   try {
-    const comments = await Comment.find({}).sort({ createdAt: -1 });
+    const comments = await Comment.find({}).sort({ createdAt: -1 }).populate('userId', 'username');
     res.json(comments);
   } catch (err) {
     next(err);
